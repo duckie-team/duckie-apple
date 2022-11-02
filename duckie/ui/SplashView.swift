5f47117657c359cef2ac325ae5582d406b227bc3
goddoro <dongsang1222@daum.net>
2022-11-02 14:45:49 +0900

    feat: Splash 화면 그리기

---
+++ duckie/ui/SplashView.swift
@@ -0,0 +1,28 @@
+//
+//  SplashView.swift
+//  duckie
+//
+//  Created by DongHo Lee on 2022/11/02.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        ZStack {
+            Color("DuckieOrange").edgesIgnoringSafeArea(.all)
+            Image("logo")
+                .resizable()
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+                .frame(width: 96 , height: 96)
+        }
+
+    }
+}
+
+struct Splashview_Preview: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
