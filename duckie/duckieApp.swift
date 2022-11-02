2a49c014a0068725fb0ccd8702297bf88cbb8874
goddoro <dongsang1222@daum.net>
2022-11-02 14:01:44 +0900

    Initial Commit

---
+++ duckie/duckieApp.swift
@@ -0,0 +1,17 @@
+//
+//  duckieApp.swift
+//  duckie
+//
+//  Created by DongHo Lee on 2022/11/02.
+//
+
+import SwiftUI
+
+@main
+struct duckieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
