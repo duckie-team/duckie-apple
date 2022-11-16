f86911b85895f7923029ba7f340654038ed2d660
goddoro <dongsang1222@daum.net>
2022-11-16 14:49:54 +0900

    feat: Design Systen Color 추가

--- duckie/extension/Color.swift
+++ duckie/extension/Color.swift
@@ -9,5 +9,12 @@ import SwiftUI
 
 extension Color {
     static let DuckieOrange = Color("DuckieOrange")
+    static let Black = Color("Black")
+    static let Gray1 = Color("Gray1")
+    static let Gray2 = Color("Gray2")
+    static let Gray3 = Color("Gray3")
+    static let Gray4 = Color("Gray4")
+    static let Alert = Color("Alert")
+    static let White = Color("White")
     
 }
