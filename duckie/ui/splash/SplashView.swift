22179100b502b7fcc1bf8903ab70cf47de7f05cb
goddoro <dongsang1222@daum.net>
2022-11-16 14:51:54 +0900

    feat: splash쪽 화면 구성 완료

---
+++ duckie/ui/splash/SplashView.swift
@@ -0,0 +1,25 @@
+//
+//  SplashView.swift
+//  duckie
+//
+//  Created by DongHo Lee on 2022/11/02.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        ZStack {
+            Color.DuckieOrange.edgesIgnoringSafeArea(.all)
+            Image.Logo
+                .frame(width: 96 , height: 96)
+        }
+
+    }
+}
+
+struct Splashview_Preview: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
