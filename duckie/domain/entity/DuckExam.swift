e3798d8730a031f02b4c784264e79a06ac6862be
goddoro <dongsang1222@daum.net>
2022-11-16 19:41:20 +0900

    refac: DuckExam 모델링

---
+++ duckie/domain/entity/DuckExam.swift
@@ -0,0 +1,17 @@
+//
+//  DuckExam.swift
+//  duckie
+//
+//  Created by DongHo Lee on 2022/11/16.
+//
+
+import SwiftUI
+
+struct DuckExam {
+    let id: Int
+    let title: String
+    let description: String
+    let buttonTitle: String
+    let hasMusic: Bool
+    let thumbnail: String = ""
+}
--- duckie/ui/home/recommend/HomeRecommendView.swift
+++ duckie/ui/home/recommend/HomeRecommendView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct HomeRecommendView: View {
     var body: some View {
-        let items = ["3","4","5"]
+        let items = getMockMainItems()
         VStack {
             HStack {
                 TabView{
-                    ForEach(items.indices) { index in
+                    ForEach(items, id: \.id) { item in
                         VStack{
-                            HomeMainExamItem()
+                            HomeMainExamItem(
+                                item : item
+                            )
                         }
                     }
                 }
@@ -31,21 +33,28 @@ struct HomeRecommendView: View {
     }
     
     @ViewBuilder
-    private func HomeMainExamItem() ->  some View {
-        
+    private func HomeMainExamItem(
+        item: DuckExam
+    ) ->  some View {
         VStack {
             Image("sample_main_image").frame(width: UIScreen.main.bounds.width - 32, height: 240).cornerRadius(12)
             Spacer().frame(height: 16)
-            Headline1(text:"제 1회 도로 패션영역")
+            Headline1(text: item.title)
             Spacer().frame(height: 16)
-            Body1(text: "아 저 근데 너무 재밌을 것 같아요\n내 시험 최고", textAlign: .center)
+            Body1(text: item.description, textAlign: .center)
             Spacer().frame(height: 24)
-            RoundedLargeButton(title: "어떻게 풀어보자~", onClick: {}, enabled : true).padding(16)
+            RoundedLargeButton(title: item.buttonTitle, onClick: {}, enabled : true).padding(16)
             Spacer().frame(height:4)
-            Body3(text: "본 고사는 듣기 평가가 포함되어 있습니다. 미리 음량을 조절해주세요.")
-            
+            if (item.hasMusic) {
+                Body3(text: item.title)
+            }
         }
-
+    }
+    private func getMockMainItems() -> [DuckExam] {
+        return [DuckExam(id: 0,title: "제 1회 도로 패션영역", description: "아 저 근데 너무 재밌을거 같아요\n내 시험 최고", buttonTitle: "어떻게 풀어보자~", hasMusic: true),
+                DuckExam(id: 1,title: "제 1회 도로 패션영역", description: "아 저 근데 너무 재밌을거 같아요\n내 시험 최고", buttonTitle: "어떻게 풀어보자~", hasMusic: true),
+                DuckExam(id: 2,title: "제 1회 도로 패션영역", description: "아 저 근데 너무 재밌을거 같아요\n내 시험 최고", buttonTitle: "어떻게 풀어보자~", hasMusic: true),
+        ]
     }
 }
 
