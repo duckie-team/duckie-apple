208a851b89efb557f414e63b79c98bfc0f293de4
goddoro <dongsang1222@daum.net>
2022-11-16 15:08:37 +0900

    feat: Home에서 팔로잉과 추천으로 나눠놓기

--- duckie/ui/home/HomeView.swift
+++ duckie/ui/home/HomeView.swift
@@ -8,39 +8,52 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State var currentSelectedIndex = 1
+    @State var currentSelectedIndex = 0
     
     var body: some View {
-        if ( currentSelectedIndex == 0 ){
-            Text("Hello My World!").onTapGesture {
-                currentSelectedIndex = 1
+        VStack(alignment: .leading){
+            HomeTopAppBar(currentSelectedIndex: currentSelectedIndex) { index in
+                currentSelectedIndex = index
             }
-        } else {
-            Text("Hello, World!").onTapGesture {
-                currentSelectedIndex = 0
+            if(currentSelectedIndex == 0){
+                HomeRecommendView()
             }
-        }
+            else {
+                HomeFollowingView()
+            }
+        }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
     }
     
     @ViewBuilder
     private func HomeTopAppBar(
-        onClick: @escaping () -> Void
+        currentSelectedIndex: Int,
+        onClick: @escaping (Int) -> Void
     ) -> some View {
         HStack{
-            Headline2(text: <#T##String#>)
+            HomeTopMenu(index: 0, title: "추천", isSelected: currentSelectedIndex == 0) { index in
+                onClick(index)
+            }
+            HomeTopMenu(index: 1, title: "팔로잉", isSelected: currentSelectedIndex == 1){ index in
+                onClick(index)
+            }
         }
     }
     
     @ViewBuilder
     private func HomeTopMenu(
+        index: Int,
         title: String,
         isSelected: Bool,
-        onClick: @escaping() -> Void
+        onClick: @escaping (Int) -> Void
     ) -> some View {
         if(isSelected){
-            Headline2(text: title, color: Color.Black)
+            Headline2(text: title, color: Color.Black).onTapGesture {
+                onClick(index)
+            }
         } else {
-            Title2(text: title, color: Color.Gray2)
+            Title2(text: title, color: Color.Gray2).onTapGesture {
+                onClick(index)
+            }
         }
     }
 }
--- duckie/ui/typography.swift
+++ duckie/ui/typography.swift
@@ -7,39 +7,39 @@
 
 import SwiftUI
 
-func Headline1(text: String, color: Color) -> some View {
+func Headline1(text: String, color: Color = Color.Black) -> some View {
     Text(text).font(.system(size: 20, weight: .bold, design: .rounded)).kerning(-0.4).foregroundColor(color)
 }
 //
-func Headline2(text: String, color: Color) -> some View {
+func Headline2(text: String, color: Color = Color.Black) -> some View {
     Text(text).font(.system(size: 16, weight: .bold, design: .rounded)).kerning(-0.4).foregroundColor(color)
 }
 
-func Title1(text: String, color: Color) -> some View {
+func Title1(text: String, color: Color = Color.Black) -> some View {
     Text(text).font(.system(size: 16, weight: .regular)).kerning(-0.4).foregroundColor(color)
 }
 
-func Title2(text: String, color: Color) -> some View {
+func Title2(text: String, color: Color = Color.Black) -> some View {
     Text(text).font(.system(size: 14, weight: .bold, design: .rounded)).kerning(0.2).foregroundColor(color)
 }
 
-func Subtitle(text: String, color: Color) -> some View {
+func Subtitle(text: String, color: Color = Color.Black) -> some View {
     Text(text).font(.system(size: 14, weight: .medium)).kerning(0.2).foregroundColor(color)
 }
 
-func Subtitle2(text: String, color: Color) -> some View {
+func Subtitle2(text: String, color: Color = Color.Black) -> some View {
     Text(text).font(.system(size: 12, weight: .bold, design: .rounded)).kerning(0.4).foregroundColor(color)
 }
 
-func Body1(text: String, color: Color) -> some View {
+func Body1(text: String, color: Color = Color.Black) -> some View {
     Text(text).font(.system(size: 14, weight: .regular)).kerning(0.2).foregroundColor(color)
 }
 
-func Body2(text: String, color: Color) -> some View {
+func Body2(text: String, color: Color = Color.Black) -> some View {
     Text(text).font(.system(size: 12, weight: .regular)).kerning(0.2).foregroundColor(color)
 }
 
-func Body3(text: String, color: Color) -> some View {
+func Body3(text: String, color: Color = Color.Black) -> some View {
     Text(text).font(.system(size: 10, weight: .regular)).kerning(0.4).foregroundColor(color)
 }
 
